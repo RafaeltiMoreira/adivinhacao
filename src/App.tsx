63adb95ffdee2976117a539eaeb5760cf3acd2e7
Rafael Moreira <rafaeltimoreirass@gmail.com>
2025-04-18 01:54:17 -0300

    Implementação das funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,150 @@
+import styles from "./app.module.css";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { useCallback, useEffect, useState } from "react";
+
+import { WORDS, Challenge } from "./utils/words";
+
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
 import { Input } from "./components/Input";
 import { Button } from "./components/Button";
+import { LettersUsed, LettersUsedProps } from "./components/LettersUsed";
 
-import styles from "./app.module.css";
-import { LettersUsed } from "./components/LettersUsed";
+const ATTEMPTS_MAX = 5;
 
 export default function App() {
+  const [score, setScore] = useState(0);
+  const [letter, setLetter] = useState("");
+  const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([]);
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+
+  // function handleRestartGame() {
+  //   const isConfirmed = window.confirm("Você realmente deseja reiniciar o jogo?");
+  //   if (isConfirmed) {
+  //     startGame();
+  //   }
+  // }
   function handleRestartGame() {
-    alert("Reiniciar o jogo");
+    startGame();
+    toast.info("Jogo reiniciado.");
+  }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+    setChallenge(randomWord);
+    setScore(0);
+    setLetter("");
+    setLettersUsed([]);
+  }
+
+  function handleConfirm() {
+    if (!challenge) {
+      return;
+    }
+
+    if (!letter.trim()) {
+      toast.warn("Digite uma letra");
+    }
+
+    const value = letter.toUpperCase();
+    const exists = lettersUsed.find((used) => used.value.toUpperCase() === value);
+
+    if (exists) {
+      setLetter("");
+      return toast.info("Você já utilizou a letra " + value);
+    }
+
+    const hits = challenge.word
+      .toUpperCase()
+      .split("")
+      .filter((char) => char === value).length;
+
+    const correct = hits > 0;
+    const currentScore = score + hits
+
+    setLettersUsed((prevState) => [...prevState, { value, correct }])
+    setScore(currentScore);
+    setLetter("");
+  }
+
+  const endGame = useCallback((message: string, success = true) => {
+    if (success) {
+      toast.success(message);
+    } else {
+      toast.error(message);
+    }
+    
+    setTimeout(() => startGame(), 2000);
+  }, []);
+
+  useEffect(() => {
+    startGame();
+  }, [])
+
+  useEffect(() => {
+    if (!challenge) {
+      return;
+    }
+
+    const timeout =setTimeout(() => {
+      if (score === challenge.word.length) {
+        return endGame("Parabéns, você adivinhou a palavra!", true);
+      }
+
+      const attemptLimit = challenge.word.length + ATTEMPTS_MAX;
+      if (lettersUsed.length === attemptLimit) {
+        return endGame("Que pena, você usou todas as tentativas. Tente novamente!", false);
+      }
+    }, 200)
+
+    return () => clearTimeout(timeout);
+  }, [score, lettersUsed.length, challenge, endGame]);
+
+  if (!challenge) {
+    return
   }
 
   return (
     <>
       <div className={styles.container}>
         <main>
-          <Header current={5} max={10} onRestart={handleRestartGame} />
-          <Tip tip="Uma das linguagens de programação mais utilizadas." />
+          <Header 
+            current={lettersUsed.length} 
+            max={challenge.word.length + ATTEMPTS_MAX} 
+            onRestart={handleRestartGame} 
+          />
+          <Tip tip={challenge.tip} />
           <div className={styles.word}>
-            <Letter value="R" />
-            <Letter value="E" />
-            <Letter value="A" />
-            <Letter value="C" />
-            <Letter value="T" />
+            {
+              challenge.word.split("").map((letter, index) => {
+                const letterUsed = lettersUsed.find((used) => used.value.toUpperCase() === letter.toUpperCase());
+
+                return (<Letter key={index} value={letterUsed?.value} color={letterUsed?.correct ? "correct" : "default"} />
+                )
+              })
+            }
           </div>
 
           <h4>Palpite</h4>
 
           <div className={styles.guess}>
-            <Input autoFocus maxLength={1} placeholder="?" />
-            <Button title="Confirmar" />
+            <Input
+              autoFocus
+              maxLength={1}
+              placeholder="?"
+              value={letter}
+              onChange={(e) => setLetter(e.target.value)}
+            />
+            <Button title="Confirmar" onClick={handleConfirm} />
           </div>
 
-          <LettersUsed />
+          <LettersUsed data={lettersUsed} />
         </main>
       </div>
+      <ToastContainer position="top-center" />
     </>
   )
 }
