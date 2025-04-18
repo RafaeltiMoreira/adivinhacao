b6ccba6d1089e57d4f11e175bfa131d712f5f5b1
Rafael Moreira <rafaeltimoreirass@gmail.com>
2025-04-17 23:26:50 -0300

    Implementação da Interface da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header";
+import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
 
 import styles from "./app.module.css";
+import { LettersUsed } from "./components/LettersUsed";
 
 export default function App() {
   function handleRestartGame() {
@@ -12,6 +17,23 @@ export default function App() {
       <div className={styles.container}>
         <main>
           <Header current={5} max={10} onRestart={handleRestartGame} />
+          <Tip tip="Uma das linguagens de programação mais utilizadas." />
+          <div className={styles.word}>
+            <Letter value="R" />
+            <Letter value="E" />
+            <Letter value="A" />
+            <Letter value="C" />
+            <Letter value="T" />
+          </div>
+
+          <h4>Palpite</h4>
+
+          <div className={styles.guess}>
+            <Input autoFocus maxLength={1} placeholder="?" />
+            <Button title="Confirmar" />
+          </div>
+
+          <LettersUsed />
         </main>
       </div>
     </>
